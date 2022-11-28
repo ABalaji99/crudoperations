2d42a8be5c661b9436fd106178bd742cb7137e99
A Balaji <amulunarayanabalaji@gmail.com>
2022-11-28 13:15:39 +0530

    ok

---
+++ src/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Navigation = () => {
+  return (
+    
+        <nav>
+           <a href="#">Home</a>
+        </nav>
+    
+  )
+}
+
+export default Navigation
